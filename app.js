2498187e70c155b3f9f143f80efc411cc1faf1bf
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-08-27 00:00:34 +0200

    Full suite of modules installed, session configured, cors set up

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./config/database.config')
 
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -8,16 +9,26 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors')
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
-
-mongoose
-  .connect('mongodb://localhost/medplan-server', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+app.use(
+  session({
+    secret: 'my-secret-weapon',
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+      maxAge: 60 * 60 * 24 * 1000, //60 sec * 60 min * 24hrs = 1 day (in milliseconds)
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      //time to live (in seconds)
+      ttl: 60 * 60 * 24,
+      autoRemove: 'disabled',
+    }),
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+);
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -29,6 +40,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors({
+  credentials:true,
+  origin: ['http://localhost:3000']
+}))
 
 // Express View engine setup
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcryptjs');
+const {PatientModel, DoctorModel} = require('../models/User.models');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
