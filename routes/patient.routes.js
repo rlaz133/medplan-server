69ed9b275e8f9912ac9701d9d8c46537972ae161
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-08-28 14:12:48 +0200

    Models expanded. Patient routes for profile done. Halfway there on appointments

--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 const {PatientModel, DoctorModel} = require('../models/User.models');
-const {AppointmentModel, PrescriptionModel} =('../model/Tools.models')
-const { isPatient } = require ('../helpers/auth.helper')
+const {AppointmentModel, PrescriptionModel} =require('../models/Tools.models')
+const { isLoggedIn, isPatient } = require ('../helpers/auth.helper')
 
 router.post('/patient/appointments/:doctorId', isPatient, (req, res)=>{
+  console.log (req.body)
+  console.log (AppointmentModel)
   AppointmentModel.create({doctor: req.params.doctorId, patient: req.session.loggedInUser._id, time: req.body.event.start})
     .then((appo)=>{res.status(200).json(appo)})
     .catch((err) => {
@@ -15,15 +17,28 @@ router.post('/patient/appointments/:doctorId', isPatient, (req, res)=>{
     })
 })
 
-router.post('/patient/appointments/:doctorId', isPatient, (req, res)=>{
-  AppointmentModel.create({doctor: req.params.doctorId, patient: req.session.loggedInUser._id, time: req.body.event.start})
-    .then((appo)=>{res.status(200).json(appo)})
+router.get('/patient/:patientId', isLoggedIn, (req, res)=>{
+  PatientModel.findById(req.params.patientId)
+    .then(patient => res.status(200).json(patient))
     .catch((err) => {
-      res.status(500).json({
-          error: 'Something went wrong',
-          message: err
-      })
+          res.status(500).json({
+              error: 'Something went wrong',
+              message: err
+          })
     })
 })
+
+router.patch('/patient/:patientId', isPatient, (req, res)=>{
+  PatientModel.findByIdAndUpdate(req.params.patientId, {$set: req.body})
+    .then(patient => res.status(200).json(patient))
+    .catch((err) => {
+          res.status(500).json({
+              error: 'Something went wrong',
+              message: err
+          })
+    })
+})
+
+
 
 module.exports = router
