15800b3318b38ec922e4f890a06fae83cc468bc3
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-09-03 00:50:33 +0300

    push to heroku

--- app.js
+++ app.js
@@ -79,6 +79,14 @@ app.use('/api', patient);
 app.use('/api', doctor);
 app.use('/api', fileUpload);
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 // app.listen(process.env.PORT, () => {
 //   console.log(`Listening on http://localhost:${process.env.PORT}`);
 // });
