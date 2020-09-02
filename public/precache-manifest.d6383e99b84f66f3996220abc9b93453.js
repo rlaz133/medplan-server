302a1970c72f0e5a02cd5fec3e69c7775e9b065b
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-09-02 15:03:12 +0200

    Try to deploy

---
+++ public/precache-manifest.d6383e99b84f66f3996220abc9b93453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22778feb66723fa230b95734c50af5d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf479cb60add7d1fa94",
+    "url": "/static/css/2.c0155d99.chunk.css"
+  },
+  {
+    "revision": "e0981366ae5cc8965d45",
+    "url": "/static/css/main.6898b68c.chunk.css"
+  },
+  {
+    "revision": "fbf479cb60add7d1fa94",
+    "url": "/static/js/2.aaba2530.chunk.js"
+  },
+  {
+    "revision": "317018c4a66d072904245e1352472716",
+    "url": "/static/js/2.aaba2530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0981366ae5cc8965d45",
+    "url": "/static/js/main.9bc10a84.chunk.js"
+  },
+  {
+    "revision": "bf11f26a49cbd2896f2c",
+    "url": "/static/js/runtime-main.8d75b0aa.js"
+  }
+]);
