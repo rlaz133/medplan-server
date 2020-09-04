f06c03539ae9772b0efb49cb7d278ba94e560fef
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-09-04 11:27:21 +0200

    final deployment before presentation

--- public/precache-manifest.b8a2fba56805b0846b026b5cd5fca309.js
+++ public/precache-manifest.b8a2fba56805b0846b026b5cd5fca309.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b594bd35ab16fabe9844a4f0dd91949",
+    "revision": "bed81cbb04860eafeddef0de526bd25d",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.a2bdd2b4.chunk.css"
   },
   {
-    "revision": "e8dc08c1cbe96a12ec96",
-    "url": "/static/css/main.4055e784.chunk.css"
+    "revision": "d44c2f5a78534d742204",
+    "url": "/static/css/main.c193b8c6.chunk.css"
   },
   {
     "revision": "9d8887d5a293ce4fcc01",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6482c555.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e8dc08c1cbe96a12ec96",
-    "url": "/static/js/main.c2b39906.chunk.js"
+    "revision": "d44c2f5a78534d742204",
+    "url": "/static/js/main.0ec1e0a1.chunk.js"
   },
   {
     "revision": "bf11f26a49cbd2896f2c",
